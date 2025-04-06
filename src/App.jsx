f7b0adbdf73f6e551d1fec22f9db21eb89651633
Husky Serial <649764+smeyerhuky@users.noreply.github.com>
2025-04-06 00:02:32 -0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,13 @@ import './App.css'
 import MonteCarloTutorial from './components/monte/MonteCarloTutorial'
 import MonteCarloProjectSimulator from './components/monte/MonteCarloProjectSimulator'
 import MonteCarloAdvancedTutorial from './components/monte/MonteCarloAdvancedTutorial'
-
+import InteractivePMTensor from './components/monte/InteractivePMTensor'
 // Navigation items configuration
 const navigationItems = [  
   { id:'sim', label: 'Simulator', component: MonteCarloProjectSimulator },
   { id: 'tut', label: 'Tutorial', component: MonteCarloTutorial }, 
   { id: 'adv_tut', label: 'Advanced Monte Carlo Tutorial', component: MonteCarloAdvancedTutorial },
+  { id: 'tensors', label: 'An example of a tensor matrix', component: InteractivePMTensor },
 ]
 
 // Navigation component
