7605dc86f80e5d200bcebdbbf88488064ca15309
Husky Serial <649764+smeyerhuky@users.noreply.github.com>
2025-04-07 23:58:49 -0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import './App.css'
 import MonteCarloTutorial from './components/monte/MonteCarloTutorial'
 import MonteCarloProjectSimulator from './components/monte/MonteCarloProjectSimulator'
 import MonteCarloAdvancedTutorial from './components/monte/MonteCarloAdvancedTutorial'
-import InteractivePMTensor from './components/monte/InteractivePMTensor'
+// import InteractivePMTensor from './components/monte/InteractivePMTensor'
 import Calendar from './components/Calendar'
 
 
@@ -13,7 +13,7 @@ const navigationItems = [
   { id:'sim', label: 'Simulator', component: MonteCarloProjectSimulator },
   { id: 'tut', label: 'Tutorial', component: MonteCarloTutorial }, 
   { id: 'adv_tut', label: 'Advanced Monte Carlo Tutorial', component: MonteCarloAdvancedTutorial },
-  { id: 'tensors', label: 'An example of a tensor matrix', component: InteractivePMTensor },
+//  { id: 'tensors', label: 'An example of a tensor matrix', component: InteractivePMTensor },
   { id: 'cal', label: 'Calendar Demo', component: Calendar },
 ]
 
