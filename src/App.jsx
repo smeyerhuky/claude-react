5d47163153005a78adc1f6e65a1c228fd030a270
Husky Serial <649764+smeyerhuky@users.noreply.github.com>
2025-05-20 10:26:57 -0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@
 import { Routes, Route, useParams, useNavigate, useLocation, Link, Navigate } from 'react-router-dom'
 import './App.css'
 
-import MediterraneanMealPlanner from './components/medi/MediterraneanMealPlanner'
+import MediterraneanMealPlanner from './components/meid/MediterraneanMealPlanner'
 import MonteCarloProjectSimulator from './components/monte/MonteCarloProjectSimulator'
 import MonteCarloAdvancedTutorial from'./components/monte/MonteCarloAdvancedTutorial'
 import MatrixExplorer from './components/MatrixExplorer'
